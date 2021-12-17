4561f58509657a2fab8bea1aeba78306e5e7a8d4
Techpro7 <tripathipranjal75@gmail.com>
2021-12-17 20:25:57 +0530

    adding pid controller

---
+++ aiitra/src/scripts/pid_controller_omni.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include"stdafx.h"
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+#include <ros/ros.h>
+
+
+using namespace std;
+
+/*defining global parameters*/
+global double g=9.81,m=3.50,Izz=0.07;
+global double Kp=1,Kd=1;
+global double t_res=0.001,k=0.1;
+
+/*defining the controller function*/
+vector<double> error_pos;
+vector<double> error_vel;
+vector<double> desired_state_pos;
+vector<double> current_state_pos;
+vector<double> desired_state_vel;
+vector<double> current_state_vel;
+vector<double> desired_state_acc;
+vector<double> current_state_acc;
+
+
+for(int i=0;i<3;i++)
+{
+ error_pos[i]=current_state_pos[i]-desired_state_pos[i];
+ error_vel[i]=current_state_vel[i]-desired_state_vel[i];
+ 
+ 
+}
+
+
+vector<double> controller(vector<double> error_pos;vector<double> error_vel;vector<double> desired_state_acc)
+{
+/**defining the conrol law*/
+vector<double> current_state_acc;
+double f[3];//forces on the wheel of the bot
+vector<double> omega_wheel;
+for(int i=0;i<3;i++)
+ {current_state_acc[i]=desired_state_acc[i]+ Kp*error_pos[i] +Kd*error_vel[i];}
+vector<vector<double>> inverse_matrix;
+inverse_matrix[0][0]=-1.1547344;
+inverse_matrix[0][1]=1.333;
+inverse_matrix[0][2]=0.3333;
+inverse_matrix[1][0]=0;
+inverse_matrix[1][1]=-0.6666;
+inverse_matrix[1][2]=0.333;
+inverse_matrix[2][0]=1.1547344;
+inverse_matrix[2][1]=-0.6666;
+inverse_matrix[2][2]=0.333;
+
+for(int i=0;i<3;i++)
+{
+ f[i]=((m*current_state_acc[0])*inverse_matrix[i][0]+(m*current_state_acc[1])*inverse_matrix[i][1]+(Izz*current_state_acc[2])*inverse_matrix[i][2])/d;
+}
+omega_wheel.push_back(sqrt(f[0]/k));
+omega_wheel.push_back(sqrt(f[1]/k));
+omega_wheel.push_back(sqrt(f[2]/k));
+
+}
+
+
+
